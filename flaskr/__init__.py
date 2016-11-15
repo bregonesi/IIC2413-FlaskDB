181d3706195399effd0bcb82db593e55c9617241
bregonesi <rego_1994@hotmail.com>
2016-11-15 17:03:03 -0300

    s

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -17,7 +17,7 @@ def create_app():
 app = create_app()
 
 # REPLACE WITH YOUR DATABASE NAME
-MONGODATABASE = "users"
+MONGODATABASE = "e4"
 MONGOSERVER = "localhost"
 MONGOPORT = 27017
 client = MongoClient(MONGOSERVER, MONGOPORT)
